b7d5edfeb1b4938b7964c0a33ed648dcc8dbb6ee
loveforall1 <96542932+loveforall1@users.noreply.github.com>
2023-01-04 16:34:55 +0330

    Update script.js

--- script.js
+++ script.js
@@ -7,39 +7,41 @@ let tempElem = $.querySelector(".temp")
 let weatherElem = $.querySelector(".weather")
 let hi_LowElem = $.querySelector(".hi-low")
 
-const fetchURL = "https://api.openweathermap.org/data/2.5/weather?q="
+const fetchURL = "http://api.weatherstack.com/current?access_key="
 let cityName = null
-const apiKey = "05017894b8b0ac83af72659f3dc9d03c"
+const apiKey = "bf2a9580b7c71ddedcea83a6c9861925"
 
 searchInput.addEventListener("keyup", event =>{
-  let {keyCode, target:input} = event
-  if(keyCode === 13){
-    cityName = input.value
-    weatherData(cityName)
-  }
+	let {keyCode, target:input} = event
+	if(keyCode === 13){
+		cityName = input.value
+		weatherData(cityName)
+	}
 })
 
 function weatherData(CityName) {
-  fetch(`${fetchURL}${CityName}&appid=${apiKey}`)
-    .then(res => res.json())
-    .then(cityData => {
-      console.log(cityData)
-      cityElem.innerHTML = `${cityData.name}, ${cityData.sys.country}`
-      dateElem.innerHTML = mainDate()
-      tempElem.innerHTML = `${Math.floor(cityData.main.temp - 273.15)}<span>°c</span>`
-      weatherElem.innerHTML = `${cityData.weather[0].main}`
-      hi_LowElem.innerHTML = `${Math.floor(cityData.main.temp_min - 273.15)}°c / ${Math.floor(cityData.main.temp_max - 273.15)}°c`
-    })
-    .catch(err => alert("city not found"))
+	fetch(`${fetchURL}${apiKey}&query=${CityName}`)
+		.then(res => res.json())
+		.then(cityData => {
+		 	console.log(cityData)
+			cityElem.innerHTML = `${cityData.location.name}, ${cityData.location.country}`
+		 	dateElem.innerHTML = mainDate()
+		 	
+		 	tempElem.innerHTML = `${cityData.current.temperature}<span>°c</span>`
+		 	weatherElem.innerHTML = `${cityData.current.weather_descriptions[0]}<img src="${cityData.current.weather_icons[0]}" alt="weather_icon" title="${cityData.current.weather_descriptions[0]}">`
+		 	
+		 	hi_LowElem.innerHTML = `${cityData.current.temperature}°c / ${cityData.current.feelslike}°c`
+		})
+		.catch(err => alert("city not found"))
 }
 
 function mainDate() {
-  let weekArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  let monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-  let localDate = new Date()
-  let day = localDate.getDate()
-  let week =  weekArray[localDate.getDay()]
-  let month = monthArray[localDate.getMonth()]
-  let year = localDate.getFullYear()
-  return `${week} ${day} ${month} ${year}`
+	let weekArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+	let monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	let localDate = new Date()
+	let day = localDate.getDate()
+	let week =  weekArray[localDate.getDay()]
+	let month = monthArray[localDate.getMonth()]
+	let year = localDate.getFullYear()
+	return `${week} ${day} ${month} ${year}`
 }
